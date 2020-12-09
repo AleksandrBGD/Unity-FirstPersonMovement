6f51177cfdfa94f13e8b67a67c950eb4e9a1cd7b
Piton <BotUnityGameDev@gmail.com>
2020-12-09 18:51:08 +0300

    1.Movement & Rotation
    First Person Movement - Movement & Rotation

---
+++ Assets/1. FirstPersonMovement - Movement&Rotation/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using BGD.FirsPersonMovement;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _camera;
+    [SerializeField] private MovementCharacteristics _characteristics;
+    [SerializeField] private PoolKeyInput _input;
+
+    private float _horizontal, _vertical;
+    private float _mouseX, _mouseY;
+
+    private float _rotX, _rotY;// Vector2
+
+    private Vector3 _dir;
+
+
+    private CharacterController _controller;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        _rotX = rot.x;
+        _rotY = rot.y;
+
+        Cursor.visible = _characteristics.VisibleCursor;
+    }
+
+
+    private void Update()
+    {
+        Movement();
+        Rotate();
+    }
+
+    private void Movement()
+    {
+        if(_controller.isGrounded)
+        {
+            _horizontal = Input.GetAxis(_input.Horizontal);
+            _vertical = Input.GetAxis(_input.Vertical);
+
+            _dir = transform.TransformDirection(_horizontal, 0, _vertical).normalized;
+
+        }
+
+        _dir.y -= _characteristics.Gravity * Time.deltaTime;
+
+        _controller.Move(_dir * _characteristics.MovementSpeed * Time.deltaTime);
+    }
+
+    private void Rotate()
+    {
+        _mouseX = Input.GetAxis(_input.MouseX);
+        _mouseY = Input.GetAxis(_input.MouseY);
+
+        _rotX -= _characteristics.AngularSpeed * _mouseY * Time.deltaTime;
+        _rotY += _characteristics.AngularSpeed * _mouseX * Time.deltaTime;
+
+        _rotX = Mathf.Clamp(_rotX, -_characteristics.ClampAngleX, _characteristics.ClampAngleX);
+
+        Quaternion rotY = Quaternion.Euler(0, _rotY, 0);
+        Quaternion localRotX = Quaternion.Euler(_rotX, 0, 0);
+
+        transform.rotation = rotY;
+        _camera.localRotation = localRotX;
+
+    }
+}
---
+++ Assets/1. FirstPersonMovement - Movement&Rotation/Scripts/ScriptableObjects/PoolKeyInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace BGD.FirsPersonMovement
+{
+    [CreateAssetMenu(fileName = "Key", menuName = "Key/PoolKeyInput", order = 1)]
+    public class PoolKeyInput : ScriptableObject
+    {
+        [SerializeField] private string _vertical = "input";
+        [SerializeField] private string _horizontal = "input";
+
+        [SerializeField] private string _run = "input";
+        [SerializeField] private string _jump = "input";
+        [SerializeField] private string _seet = "input";
+
+        [SerializeField] private string _mouseX = "input";
+        [SerializeField] private string _mouseY = "input";
+
+        public string Vertical => _vertical;
+        public string Horizontal => _horizontal;
+        public string Run => _run;
+        public string Jump => _jump;
+        public string Seet => _seet;
+
+        public string MouseX => _mouseX;
+        public string MouseY => _mouseY;
+    }
+}
